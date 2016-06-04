3843f8c8ee655ebf6114e1a91a8ae0ba598396e1
samirebrahim <samir.ebrahim92@gmail.com>
2016-06-04 22:43:32 +0200

    refactor controller and service

--- app/scripts/controllers/main.ctrl.js
+++ app/scripts/controllers/main.ctrl.js
@@ -11,47 +11,24 @@ angular.module('instaGithupApp')
   .controller('MainCtrl', function($scope, User, $http) {
 
 
-
     var init = function() {
       $scope.states = {};
-      User.list()
-        .then(function(response) {
-          $scope.users = response.data;
-          $scope.nextPage = parse_link_header(response.headers("Link"));
+      User.next()
+        .then(function(data) {
+          $scope.users = data;
         });
     }
-    $scope.loadMore = function() {;
-        $http.get($scope.nextPage["next"]).then(function(response) {
-          $scope.nextPage = parse_link_header(response.headers("Link"));
-            $scope.users = $scope.users.concat(response.data);
-          }
+    $scope.loadMore = function() {
+      User.next().then(function(data) {
+        $scope.users = $scope.users.concat(data);
+      })
+    }
 
-        );
-      }
-      //for the left side menu
+    //for the left side menu
     $scope.ChangeActiveUser = function(id) {
       $scope.states.activeItem = id;
     }
-    var parse_link_header = function(header) {
-      if (header.length === 0) {
-        throw new Error("input must not be of zero length");
-      }
 
-      // Split parts by comma
-      var parts = header.split(',');
-      var links = {};
-      // Parse each part into a named link
-      for (var i = 0; i < parts.length; i++) {
-        var section = parts[i].split(';');
-        if (section.length !== 2) {
-          throw new Error("section could not be split on ';'");
-        }
-        var url = section[0].replace(/<(.*)>/, '$1').trim();
-        var name = section[1].replace(/rel="(.*)"/, '$1').trim();
-        links[name] = url;
-      }
-      return links;
-    }
 
     init();
   });
--- app/scripts/services/user.service.js
+++ app/scripts/services/user.service.js
@@ -1,16 +1,21 @@
 'use strict';
 
 instaGithupApp.factory('User',
-  function($http, $state) {
+  function($http, $state,$q) {
     var User = {};
-    User.goToDefaultPage = function() {
-
-      $state.go('users.user');
-      return;
-    }
-
-    User.list = function() {
-      return $http.get('//api.github.com/users?page=1&per_page=10');
+    var nextPageLink = null;
+    User.next = function() {
+      if (nextPageLink == null) {
+        nextPageLink = '//api.github.com/users?page=1&per_page=10';
+      }
+      var deffered = $q.defer();
+      $http.get(nextPageLink).then(function(response) {
+        nextPageLink = parse_link_header(response.headers("Link"))["next"];
+        deffered.resolve(response.data);
+      }, function(response) {
+        deffered.reject(response.data);
+      });
+      return deffered.promise;
     };
 
 
@@ -19,6 +24,26 @@ instaGithupApp.factory('User',
       return $http.get('//api.github.com/user/' + userId);
     }
 
+    var parse_link_header = function(header) {
+      if (header.length === 0) {
+        throw new Error("input must not be of zero length");
+      }
+
+      // Split parts by comma
+      var parts = header.split(',');
+      var links = {};
+      // Parse each part into a named link
+      for (var i = 0; i < parts.length; i++) {
+        var section = parts[i].split(';');
+        if (section.length !== 2) {
+          throw new Error("section could not be split on ';'");
+        }
+        var url = section[0].replace(/<(.*)>/, '$1').trim();
+        var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+        links[name] = url;
+      }
+      return links;
+    }
 
     return User;
   });
